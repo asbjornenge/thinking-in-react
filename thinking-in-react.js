9b0d4b5fbb71569d5f3076c71f723c777cbf705b
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-05-18 12:30:32 +0200

    Exercises and menu.json

--- thinking-in-react.js
+++ thinking-in-react.js
@@ -7,7 +7,8 @@ Workshopper({
     name              : 'thinking-in-react'
   , title             : 'LEARN YOU THE REACT.JS THINKING FOR MUCH WIN(D)!'
   , appDir            : __dirname
+  , exerciseDir       : path.join(__dirname, '/exercises/')
   , helpFile          : path.join(__dirname, 'help.txt')
   , prerequisitesFile : path.join(__dirname, 'prerequisites.txt')
   , creditsFile       : path.join(__dirname, 'credits.txt')
-}).init()
+})
