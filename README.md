16063f45331ab215f066df8a09faaf3e7afb4261
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-05-18 12:23:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Thinking in React workshopper
+
+This is going to be a [workshopper](https://github.com/rvagg/workshopper) module based on [thinking in react](http://facebook.github.io/react/docs/thinking-in-react.html).  
+It will hopefully help you get started with [reactjs](http://facebook.github.io/react/index.html) and help you to get a good workflow from the get-go.
+
+**\*\* UNDER CONSTRUCTION \*\***
+
---
+++ thinking-in-react.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const Workshopper = require('workshopper')
+    , path        = require('path')
+
+Workshopper({
+    name              : 'thinking-in-react'
+  , title             : 'LEARN YOU THE REACT.JS THINKING FOR MUCH WIN(D)!'
+  , appDir            : __dirname
+  , helpFile          : path.join(__dirname, 'help.txt')
+  , prerequisitesFile : path.join(__dirname, 'prerequisites.txt')
+  , creditsFile       : path.join(__dirname, 'credits.txt')
+}).init()
