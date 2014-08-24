18f8e231d5575a335a5c0c64a1c46952b0a5b0ed
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-08-24 11:01:31 +0200

    Wrapping req.send in try/catch for error handling

--- bingo.js
+++ bingo.js
@@ -9,7 +9,6 @@ var box = blessed.form({
   left: 'center',
   width: '90%',
   height: '90%',
-  content: '{center}Lets play STATE BINGO!\nNavigate using UP and DOWN arrow keys.\n{/center}',
   tags: true,
   border: {
     type: 'line'
@@ -25,105 +24,105 @@ var box = blessed.form({
   }
 });
 
-box.prepend(new blessed.Text({  
+box.prepend(new blessed.Text({
   left: 2,
-  content: 'STATE-BINGO'
+  content: 'State bingo'
 }));
 
-var states = [
-  { 
-    state : 'state1',
-    top   : '10%',
-    left  : '10%'
-  },
-  { 
-    state : 'state2',
-    top   : '45%',
-    left  : '45%'
-  },
-  { 
-    state : 'state3',
-    top   : '25%',
-    left  : '30%'
-  },
-  { 
-    state : 'state4',
-    top   : '18%',
-    left  : '70%'
-  },
-  { 
-    state : 'state5',
-    top   : '80%',
-    left  : '40%'
-  },
-  { 
-    state : 'state6',
-    top   : '80%',
-    left  : '70%'
-  },
-  { 
-    state : 'state7',
-    top   : '40%',
-    left  : '20%'
-  },
-  { 
-    state : 'state8',
-    top   : '35%',
-    left  : '77%'
-  },
-  { 
-    state : 'state9',
-    top   : '67%',
-    left  : '7%'
-  },
-  { 
-    state : 'state10',
-    top   : '60%',
-    left  : '27%'
-  }
-]
 
-var cbs    = states.map(function(state, index) {
-  return blessed.checkbox({
-    left   : state.left,
-    top    : state.top,
-    width  : 15,
-    height : 3,
-    text   : state.state
-  })
+var bingoboard = blessed.box({
+  top: 8,
+  left: 'center',
+  width: '30%',
+  height: '60%',
+  tags: true,
+  border: {
+    type: 'line',
+    fg  : '#EF7702'
+  },
+  style: {
+    fg: 'white',
+  }
+});
+
+var bingologo = "\
+██████╗ ██╗███╗   ██╗ ██████╗  ██████╗ ██╗ \
+██╔══██╗██║████╗  ██║██╔════╝ ██╔═══██╗██║ \
+██████╔╝██║██╔██╗ ██║██║  ███╗██║   ██║██║ \
+██╔══██╗██║██║╚██╗██║██║   ██║██║   ██║╚═╝ \
+██████╔╝██║██║ ╚████║╚██████╔╝╚██████╔╝██╗ \
+╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═╝ \
+"
+
+box.prepend(new blessed.Text({
+    left : 'center',
+    top  : 1,
+    width : 49,
+    content: bingologo,
+    style: {
+      fg: 'red',
+    }
+}));
+
+var cbs = require('./bingostates.json').map(function(state, index) {
+    return blessed.box({
+        top     : state.top,
+        left    : state.left,
+        width   : '30%',
+        height  : '30%',
+        padding : {
+          left : '5%'    
+        },
+        content : state.state,
+        style: {
+            bg: 'blue',
+        }
+    })
 })
+
+// process.exit(0)
+
+//   return blessed.checkbox({
+//     left   : state.left,
+//     top    : state.top,
+//     width  : 15,
+//     height : 3,
+//     text   : state.state
+//   })
+// })
 cbs.forEach(function(cb) {
-  cb.key('up', function(ch, key) {
-    navigate('up')
-  })
-  cb.key('down', function(ch, key) {
-    navigate('down')
-  })
-  box.append(cb)
+  // cb.key('up', function(ch, key) {
+  //   navigate('up')
+  // })
+  // cb.key('down', function(ch, key) {
+  //   navigate('down')
+  // })
+  bingoboard.append(cb)
 })
-var cbc = 0
-var cb  = cbs[0]
+// var cbc = 0
+// var cb  = cbs[0]
 
 // Append our box to the screen.
+box.append(bingoboard)
 screen.append(box);
 
-function navigate(direction) {
-  var newcbc = function(dir) {
-    if (dir == 'up' && cbc > 0) return cbc-1
-    if (dir == 'down' && cbc < cbs.length-1) return cbc+1
-    return cbc
-  }
-
-  cbs.forEach(function(cb, index) {
-    cb.style.fg = undefined
-    if (index == newcbc(direction)) {
-      cb.style.fg = 'blue'
-      cb.focus()
-    }
-  })
-  cbc = newcbc(direction)
-  screen.render()
-}
+// function navigate(direction) {
+//   var newcbc = function(dir) {
+//     if (dir == 'up' && cbc > 0) return cbc-1
+//     if (dir == 'down' && cbc < cbs.length-1) return cbc+1
+//     return cbc
+//   }
+//
+//   cbs.forEach(function(cb, index) {
+//     cb.style.fg = undefined
+//     if (index == newcbc(direction)) {
+//       cb.style.fg = 'blue'
+//       cb.focus()
+//     }
+//   })
+//   cbc = newcbc(direction)
+//   screen.render()
+// }
 
 // Quit on Escape, q, or Control-C.
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
@@ -131,7 +130,7 @@ screen.key(['escape', 'q', 'C-c'], function(ch, key) {
 });
 
 // Focus our element.
-cb.focus();
+// cb.focus();
 
 // Render the screen.
 screen.render();
