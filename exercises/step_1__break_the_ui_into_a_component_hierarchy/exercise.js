abff1f8d05deb36c09c2e58c65abe80a779a8f22
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-08-29 09:53:12 +0200

    Prepared the step 1 presentation module for workshopper

--- exercises/step_1__break_the_ui_into_a_component_hierarchy/exercise.js
+++ exercises/step_1__break_the_ui_into_a_component_hierarchy/exercise.js
@@ -1,105 +1,54 @@
-var exercise    = function() {
-        return(
-            {
-                intro   : "Press {blue-fg}[ENTER]{/blue-fg} to start",
-                steps   :
-                    [
-                        {
-                            title       : "Target layout",
-                            description : "1) This layout forms the basis for our component structure. In the next steps we will follow the logical component breakdown.",
-                            treePrepend : [],
-                            size        : {
-                                width   : 36,
-                                height  : 13
-                            },
-                            placement   : {
-                                left    : 4,
-                                top     : 4
-                            }
-                        },
-                        {
-                            title       : "FilterableProductTable",
-                            description : "2) This is the outmost component, and contains everything.",
-                            treePrepend : [
-                                " ",
-                                "  "
-                            ],
-                            size        : {
-                                width   : 36,
-                                height  : 13
-                            },
-                            placement   : {
-                                left    : 4,
-                                top     : 4
-                            }
-                        },
-                        {
-                            title       : "SearchBar",
-                            description : "3) This component will be used for the search bar.",
-                            treePrepend : [
-                                "  |---",
-                                "  "
-                            ],
-                            size        : {
-                                width   : 34,
-                                height  : 3
-                            },
-                            placement   : {
-                                left    : 5,
-                                top     : 5
-                            }
-                        },
-                        {
-                            title       : "ProductTable",
-                            description : "4) This component will contain the table.",
-                            treePrepend : [
-                                "  |---",
-                                "  "
-                            ],
-                            size        : {
-                                width   : 34,
-                                height  : 8
-                            },
-                            placement   : {
-                                left    : 5,
-                                top     : 8
-                            }
-                        },
-                        {
-                            title       : "ProductCategoryRow",
-                            description : "5) This component will be used for each product category row.",
-                            treePrepend : [
-                                "       |---",
-                                "       "
-                            ],
-                            size        : {
-                                width   : 32,
-                                height  : 3
-                            },
-                            placement   : {
-                                left    : 6,
-                                top     : 9
-                            }
-                        },
-                        {
-                            title       : "ProductRow",
-                            description : "6) This component will be used for each product row. That's it!",
-                            treePrepend : [
-                                "       |---",
-                                "       "
-                            ],
-                            size        : {
-                                width   : 32,
-                                height  : 3
-                            },
-                            placement   : {
-                                left    : 6,
-                                top     : 12
-                            }
-                        }
-                    ]
-            }
-        )
-    };
+var exercise      = require('workshopper-exercise')()
+  , filecheck     = require('workshopper-exercise/filecheck')
+  , execute       = require('workshopper-exercise/execute')
+  , concat        = require('concat-stream')
+  , chalk         = require('chalk')
+  , presentation  = require('./presentation')
 
-module.exports = exercise;
+// checks that the submission file actually exists
+// exercise = filecheck(exercise)
+
+// execute the solution and submission in parallel with spawn()
+exercise = execute(exercise)
+
+// exercise = comparestdout(exercise)
+
+exercise.addProcessor(function (mode, callback) {
+    presentation(function (passed) {
+        callback(null, passed)
+    })
+    // if (mode == 'run')    { this.submissionStdout.pipe(concat(function(data) { console.log(data.toString())})) }
+    // if (mode == 'verify') { callback(null, true) }
+})
+
+// exercise.addVerifyProcessor(function (callback) {
+//     console.log('\n'+chalk.yellow('Your submission results compared to the expected:')+'\n')
+//
+//     var required_passes = 2
+//     var passes          = 0
+//     var score = function(matched) {
+//         passes++
+//         this.emit('pass', matched+" is correct!")
+//     }.bind(this)
+//
+//     var write = concat(function(data) {
+//         var output = data.toString()
+//         if (!passed) {
+//             this.emit('fail', passes+' found, one or more still missing.')
+//         }
+//         if (output.split('\n').length > required_passes+1) {
+//             this.emit('fail', 'Too many states there mate! DRY...')
+//             passed = false
+//         }
+//         callback(null, passed)
+//     }.bind(this))
+//     this.submissionStdout.pipe(write)
+//
+// })
+
+exercise.addCleanup(function (mode, passed, callback) {
+    console.log(chalk.green('Press ESC to return to your terminal.'))
+    // if (mode == 'verify' && passed) printer.file(this.workshopper.appName, this.workshopper.appDir, this.workshopper.appDir+'/exercises/final_words.md')
+})
+
+module.exports = exercise
