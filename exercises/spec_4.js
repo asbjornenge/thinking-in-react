56711244bf17a8e48a806433af7e2c21caf8b422
jakubsadura <jakubsadura@users.noreply.github.com>
2017-03-28 02:41:45 +0200

    Update spec_4.js
    Strict unequality is needed since the initial `filterText` value can be `null` which makes this test fail (since `null == undefined`)

--- exercises/spec_4.js
+++ exercises/spec_4.js
@@ -13,8 +13,8 @@ describe('Stateful', function() {
     it('state should live at FilterableProductTable', function(done) {
         var _comp = render(function() {
             var _filterableProductTable = ReactTestUtils.findRenderedComponentWithType(_comp, Solution.FilterableProductTable)
-            assert(_filterableProductTable.state.filterText != undefined)
-            assert(_filterableProductTable.state.inStockOnly != undefined)
+            assert(_filterableProductTable.state.filterText !== undefined)
+            assert(_filterableProductTable.state.inStockOnly !== undefined)
             done()
         })
     })
